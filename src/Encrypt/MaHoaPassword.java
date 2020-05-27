9e37e1f282f8c60a0ac4cd972b2b347f898d5de9
phuduy1999 <phuduy1999@gmail.com>
2020-05-27 16:29:16 +0700

    encrypt

--- src/Encrypt/MaHoaPassword.java
+++ src/Encrypt/MaHoaPassword.java
@@ -7,8 +7,7 @@ package Encrypt;
 
 import java.io.UnsupportedEncodingException;
 
-import org.apache.axis.encoding.Base64;
-
+import java.util.Base64;
 /**
  *
  * @author Administrator
@@ -19,14 +18,14 @@ public class MaHoaPassword {
     // Encode
     public static String encodeString(String text) throws UnsupportedEncodingException {
         byte[] bytes = text.getBytes("UTF-8");
-        String encodeString = Base64.encode(bytes);
+        String encodeString = Base64.getEncoder().encodeToString(bytes);
         return encodeString;
     }
 
     // Giải mã hóa một đoạn text (Đã mã hóa trước đó).
     // Decode
     public static String decodeString(String encodeText) throws UnsupportedEncodingException {
-        byte[] decodeBytes = Base64.decode(encodeText);
+        byte[] decodeBytes = Base64.getDecoder().decode(encodeText);
         String str = new String(decodeBytes, "UTF-8");
         return str;
     }
